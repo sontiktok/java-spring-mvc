3ce4768c4f82fdb9a31fd28e19be09cc1865166e
sontiktok <sonvltiktok@gmail.com>
2024-03-11 13:23:56 +0700

    Setup Spring security

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from Spring Boot!";
+        return "Hello World - Nguyen The Son";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user access page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin";
     }
 }
