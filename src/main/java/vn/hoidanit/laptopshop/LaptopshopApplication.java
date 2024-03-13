b93882801c90dbc5717c1b6201918fdb4b446994
sontiktok <sonvltiktok@gmail.com>
2024-03-13 15:55:38 +0700

    JSP

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,16 +2,15 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext hoidanit = SpringApplication.run(LaptopshopApplication.class, args);
-		for (String s : hoidanit.getBeanDefinitionNames()) {
-			System.out.println(s);
-		}
+		SpringApplication.run(LaptopshopApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,42 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomepage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("theson", test);
+        return "hello";
+    }
+
+}
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping()
+// public String getHomepage() {
+// return this.userService.handleHello();
+// }
+
+// }
