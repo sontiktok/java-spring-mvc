9360be6223d2ce3fa1ccb00b4a36b2763d84ede4
sontiktok <sonvltiktok@gmail.com>
2024-03-16 00:13:02 +0700

    #60. Repository

--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -2,9 +2,27 @@ package vn.hoidanit.laptopshop.service;
 
 import org.springframework.stereotype.Service;
 
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
+
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public String handleHello() {
         return "Hello from to service";
+    }
+
+    public User handleSaveUser(User user) {
+
+        User theson = this.userRepository.save(user);
+        System.out.println(theson);
+        return theson;
+
     }
 }
